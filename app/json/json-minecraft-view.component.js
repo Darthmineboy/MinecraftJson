02e0cfb35787bda17c32e0cca122e6f6740db34e
Darthmineboy <erik-jan@rieksen.net>
2018-02-17 00:27:02 +0100

    Add support for bold, italic, underlined, strikethrough, obfuscated

--- app/json/json-minecraft-view.component.js
+++ app/json/json-minecraft-view.component.js
@@ -3,8 +3,16 @@ angular.module('app').component('appJsonMinecraftView', {
     bindings: {
         _value: '=value'
     },
-    controller: function($scope) {
+    controller: function($scope, $interval) {
         const $ctrl = this;
+
+        var timer = $interval(function() {
+            obfuscate($ctrl.values);
+        }, 100);
+
+        $scope.$on('$destroy', function() {
+            $interval.cancel(timer);
+        });
 
         $scope.$on('update-view', function() {
             build();
@@ -13,6 +21,25 @@ angular.module('app').component('appJsonMinecraftView', {
         this.$onInit = function() {
             build();
         };
+
+        var obfuscate = function(values) {
+            angular.forEach(values, function(value) {
+                console.log(value);
+                if(value.obfuscated && value.text) {
+                    value.text = randomString(value.text.length);
+                }
+            });
+        }
+
+        function randomString(len, charSet) {
+            charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var randomString = '';
+            for (var i = 0; i < len; i++) {
+                var randomPoz = Math.floor(Math.random() * charSet.length);
+                randomString += charSet.substring(randomPoz,randomPoz+1);
+            }
+            return randomString;
+        }
 
         var traverse = function(values, parent) {
             var _values = [];
@@ -25,6 +52,24 @@ angular.module('app').component('appJsonMinecraftView', {
                     if(value.extra === parent.extra) {
                         value.extra = undefined;
                     }
+                }
+                value.styles = {};
+                if(value.bold) {
+                    value.styles['font-weight'] = 'bold';
+                }
+                if(value.italic) {
+                    value.styles['font-style'] = 'italic';
+                }
+                var textDecoration;
+                if(value.strikethrough && value.underlined) {
+                    textDecoration = 'underline line-through'
+                }else if(value.strikethrough) {
+                    textDecoration = 'line-through'
+                }else if(value.underlined) {
+                    textDecoration = 'underline'
+                }
+                if(textDecoration) {
+                    value.styles['text-decoration'] = textDecoration;
                 }
                 _values.push(value);
                 angular.forEach(traverse(value.extra, value), function(e) {
